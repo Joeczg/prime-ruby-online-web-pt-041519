441e3df38326181931713dc5043934e2ba05d9d4
zhenggao cai <caizhengao1992@gmail.com>
2019-04-20 04:50:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,9 @@
 def prime?(integer_number)
 array=[]
 array=(2..integer_number - 1).to_a
-if integer_number <= 0
+if integer_number <= 1
   return false 
-elsif integer_number == 1 || integer_number == 2 
+elsif  integer_number == 2 
   return true  
 else 
 array.all? {|number| integer_number % number != 0 }
