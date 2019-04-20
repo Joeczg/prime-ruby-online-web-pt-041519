cc2994267fce248e210226ff3b0397cbfbdd1a05
zhenggao cai <caizhengao1992@gmail.com>
2019-04-20 04:57:48 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,12 +4,13 @@ array=[]
 array=(2..integer_number - 1).to_a
 if integer_number <= 1
   return false 
-elsif  integer_number == 2 || integer_number == 3 
+elsif  integer_number == 2
   return true  
-else 
+elsif   
 array.any? {|number| integer_number % number == 0 }
   return false 
 
-
+else
+  return true 
 end 
 end
